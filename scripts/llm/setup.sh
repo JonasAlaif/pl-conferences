43220fa018ae6a0bfe91504ae99c2db51e15543f
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2025-02-03 23:33:51 +0100

    Testing

--- scripts/llm/setup.sh
+++ scripts/llm/setup.sh
@@ -7,5 +7,5 @@ export OLLAMA_NOHISTORY=true
 cd "$(dirname "$0")"
 ollama create -f Llama_curt llama3.2:curt
 ollama create -f Llama_full llama3.2:full
-ollama create -f Deepseek_curt deepseek-r1:curt
-ollama create -f Deepseek_full deepseek-r1:full
+# ollama create -f Deepseek_curt deepseek-r1:curt
+# ollama create -f Deepseek_full deepseek-r1:full
