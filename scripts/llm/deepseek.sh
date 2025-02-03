150319af1a3c672a996b8d32a77cc2c420a0b58d
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2025-02-04 00:09:17 +0100

    Use llama3.1

--- scripts/llm/deepseek.sh
+++ scripts/llm/deepseek.sh
@@ -6,11 +6,11 @@ export OLLAMA_NOHISTORY=true
 
 SEP=$'\n---\n'
 QUERY="$1$SEP$2"
-MODEL=${3:-curt}
+VERSION=${3:-curt}
 
 echo -n -e "Query (without context): $2\nAnswer: " > /dev/stderr
-OUTPUT=$(ollama run --nowordwrap "deepseek-r1:$MODEL" "$QUERY" 2> /dev/null)
-if [ "$MODEL" == "curt" ]; then
+OUTPUT=$(ollama run --nowordwrap "deepseek-r1:$VERSION" "$QUERY" 2> /dev/null)
+if [ "$VERSION" == "curt" ]; then
     OUTPUT=$(echo "$OUTPUT" | sed 's/[.]$//')
 fi
 echo -e "$OUTPUT\n" > /dev/stderr
--- scripts/llm/setup.sh
+++ scripts/llm/setup.sh
@@ -5,7 +5,7 @@
 export OLLAMA_NOHISTORY=true
 
 cd "$(dirname "$0")"
-ollama create -f Llama_curt llama3.2:curt
-ollama create -f Llama_full llama3.2:full
+ollama create -f Llama_curt llama:curt
+ollama create -f Llama_full llama:full
 # ollama create -f Deepseek_curt deepseek-r1:curt
 # ollama create -f Deepseek_full deepseek-r1:full
