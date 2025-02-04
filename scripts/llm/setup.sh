de9d1225ebd4d8af8d8f27430d34206bb4ff61f7
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2025-02-04 11:24:45 +0100

    Retry when setup

--- scripts/llm/setup.sh
+++ scripts/llm/setup.sh
@@ -6,6 +6,13 @@ export OLLAMA_NOHISTORY=true
 
 cd "$(dirname "$0")"
 ollama create -f Llama_curt llama:curt
+while [ $? -ne 0 ]; do
+    ollama create -f Llama_curt llama:curt
+done
 ollama create -f Llama_full llama:full
+while [ $? -ne 0 ]; do
+    ollama create -f Llama_full llama:full
+done
+
 # ollama create -f Deepseek_curt deepseek-r1:curt
 # ollama create -f Deepseek_full deepseek-r1:full
