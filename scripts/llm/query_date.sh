ec62e60165bee7941e229e3acf5781e198467e6a
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2025-02-05 15:14:18 +0100

    Correctly get date

--- scripts/llm/query_date.sh
+++ scripts/llm/query_date.sh
@@ -8,5 +8,5 @@ SCRIPT_DIR=$(dirname "$0" | xargs realpath)
 ANSWER=$($SCRIPT_DIR/qwen/run.sh "$1" "$2 Answer in one sentence." "$4" sentence)
 
 CONTEXT="$1${SEP}Query: $2${SEP}Response: $ANSWER"
-DATE_Q="Given the above response, format the $3 as 'YYYY-MM-DD'. No full sentence, do not write any other text in the answer. If the date was not found, answer 'No' only (one word)."
+DATE_Q="Given the above response, format the $3 as 'YYYY-MM-DD'. No full sentence, do not write any other text in the answer."
 $SCRIPT_DIR/qwen/run.sh "$CONTEXT" "$DATE_Q" "$4" curt
